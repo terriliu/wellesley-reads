8ac66c592a4a3dc6116b418783bdb8056367fef2
Terri <hliu5@wellesley.edu>
2022-04-05 17:48:57 -0400

    add flask starter

---
+++ draft/app.py
@@ -0,0 +1,83 @@
+from flask import (Flask, render_template, make_response, url_for, request,
+                   redirect, flash, session, send_from_directory, jsonify)
+from werkzeug.utils import secure_filename
+app = Flask(__name__)
+
+# one or the other of these. Defaults to MySQL (PyMySQL)
+# change comment characters to switch to SQLite
+
+import cs304dbi as dbi
+# import cs304dbi_sqlite3 as dbi
+
+import random
+
+app.secret_key = 'your secret here'
+# replace that with a random key
+app.secret_key = ''.join([ random.choice(('ABCDEFGHIJKLMNOPQRSTUVXYZ' +
+                                          'abcdefghijklmnopqrstuvxyz' +
+                                          '0123456789'))
+                           for i in range(20) ])
+
+# This gets us better error messages for certain common request errors
+app.config['TRAP_BAD_REQUEST_ERRORS'] = True
+
+@app.route('/')
+def index():
+    return render_template('main.html',title='Hello')
+
+@app.route('/greet/', methods=["GET", "POST"])
+def greet():
+    if request.method == 'GET':
+        return render_template('greet.html', title='Customized Greeting')
+    else:
+        try:
+            username = request.form['username'] # throws error if there's trouble
+            flash('form submission successful')
+            return render_template('greet.html',
+                                   title='Welcome '+username,
+                                   name=username)
+
+        except Exception as err:
+            flash('form submission error'+str(err))
+            return redirect( url_for('index') )
+
+@app.route('/formecho/', methods=['GET','POST'])
+def formecho():
+    if request.method == 'GET':
+        return render_template('form_data.html',
+                               method=request.method,
+                               form_data=request.args)
+    elif request.method == 'POST':
+        return render_template('form_data.html',
+                               method=request.method,
+                               form_data=request.form)
+    else:
+        # maybe PUT?
+        return render_template('form_data.html',
+                               method=request.method,
+                               form_data={})
+
+@app.route('/testform/')
+def testform():
+    # these forms go to the formecho route
+    return render_template('testform.html')
+
+
+@app.before_first_request
+def init_db():
+    dbi.cache_cnf()
+    # set this local variable to 'wmdb' or your personal or team db
+    db_to_use = 'put_database_name_here_db' 
+    dbi.use(db_to_use)
+    print('will connect to {}'.format(db_to_use))
+
+if __name__ == '__main__':
+    import sys, os
+    if len(sys.argv) > 1:
+        # arg, if any, is the desired port number
+        port = int(sys.argv[1])
+        assert(port>1024)
+    else:
+        port = os.getuid()
+    app.debug = True
+    app.run('0.0.0.0',port)
